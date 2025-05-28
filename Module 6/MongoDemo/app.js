d97efd0b3be96b2cbd10ca590c87dacd2d445d1a
Dennis Vickers <dennis.vickers@outlook.com>
2025-05-28 12:47:48 -0700

    M6 update

--- Module 6/MongoDemo/app.js
+++ Module 6/MongoDemo/app.js
@@ -10,13 +10,16 @@ const app = express();
 app.use(express.json());
 
 // Data model (schema)
-const tasks = require("./task");
+const data = require("./tasks");
+
 
 // Define a simple route
 app.get("/tm/tasks", async (req,res)=>{
 	try {
-		const task = await tasks.find();
-		res.status(200).json({task});
+		const task = await data.find();
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(JSON.stringify(task));
+		// res.status(200).json({task});   // Alternate method for sending response
 	} catch {
 		res.status(500).json({msg: error});
 	};
